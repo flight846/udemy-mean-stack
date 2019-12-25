bac4f4ef64913575edd4b4aa5e303617204d8ace
yazid <yazid@decision-science.com>
2019-12-25 22:08:09 +0800

    Patch edit feature (tbc)

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
   next();
 })
 
@@ -54,6 +54,35 @@ app.post('/api/posts', (req, res, next) => {
           postId: createdPost._id
       })
   })
+})
+
+app.put('/api/posts/:id', (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  })
+  Post.updateOne({
+    _id: req.params.id,
+    post
+  })
+    .then(result => {
+      console.log(result)
+      res.status(200).json({ message: 'Update successful' })
+    })
+})
+
+app.get('/api/posts/:id', (req ,res, next) => {
+  Post.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({
+          message: 'Post not found!'
+        })
+      }
+    })
 })
 
 app.delete('/api/posts/:id', (req, res, next) => {
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -38,6 +38,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts/" + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title, content };
     this.http
@@ -49,6 +53,23 @@ export class PostsService {
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
     });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {
+      id,
+      title,
+      content
+    };
+    this.http.put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe((resp) => {
+        console.log(resp)
+        // const updatedPosts = [...this.posts];
+        // const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        // updatedPosts[oldPostIndex] = post;
+        // this.posts = updatedPosts;
+        // this.postsUpdated.next([...this.posts]);
+      });
   }
 
   deletePost(postId: string) {
